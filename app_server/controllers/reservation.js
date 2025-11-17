66236d6f6cc02d2531210d9cbec7e6c72b7f396a
Austin Brancheau <austin.brancheau@gmail.com>
2025-11-16 19:45:20 -0500

    Dynamic webpage completed Milestone Two Baseline

--- app_server/controllers/reservation.js
+++ app_server/controllers/reservation.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var animals = JSON.parse(fs.readFileSync('./data/animals.json', 'utf8'));
+
 /* GET reservation view */
 const reservation = (req, res) => {
-    res.render('reservation', {title: 'Grazioso Salvare'})
+    res.render('reservation', {title: 'Grazioso Salvare', animals})
 }
 
 module.exports = {
